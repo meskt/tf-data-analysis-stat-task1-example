fa273e071ec21008b5f262ba655bdeb7023a2bb8
meskt <56514258+meskt@users.noreply.github.com>
2023-04-06 16:26:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 1841341924 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return np.mean(x) * 2 / 10 ** 2
+    return np.mean((x) / 10)
